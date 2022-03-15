9f5c3f88d57200f423344740f55f49603b208fc0
williamrolando88 <william.rolando88@hotmail.com>
2022-03-14 22:14:43 -0500

    Fix: Failing unit test

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -3,19 +3,22 @@ require_relative 'paths'
 describe 'Create music_album instance' do
   context 'giving the following valid inputs' do
     it "genre: 'Pop', on_spotify: false" do
-      album = MusicAlbum.new(genre: 'Pop', on_spotify: false)
+      pop = Genre.new(name: 'Pop')
+      album = MusicAlbum.new(genre: pop, on_spotify: false)
       expect(album).to be_instance_of MusicAlbum
-      expect(album.genre).to eq 'Pop'
+      expect(album.genre.name).to eq 'Pop'
       expect(album.on_spotify).to eq false
     end
     it "genre: 'Pop', on_spotify: true" do
-      album = MusicAlbum.new(genre: 'Pop', on_spotify: true)
-      expect(album.genre).to eq 'Pop'
+      pop = Genre.new(name: 'Pop')
+      album = MusicAlbum.new(genre: pop, on_spotify: true)
+      expect(album.genre.name).to eq 'Pop'
       expect(album.on_spotify).to eq true
     end
     it "genre: 'Jazz', on_spotify: true" do
-      album = MusicAlbum.new(genre: 'Jazz', on_spotify: true)
-      expect(album.genre).to eq 'Jazz'
+      jazz = Genre.new(name: 'Jazz')
+      album = MusicAlbum.new(genre: jazz, on_spotify: true)
+      expect(album.genre.name).to eq 'Jazz'
       expect(album.on_spotify).to eq true
       expect(album.archived).to be false
     end
